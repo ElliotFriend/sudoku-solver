f7887ea54e192593b7b77a6680a4bd13f453a85c
Elliot J. Voris <elliot@voris.me>
2021-11-28 21:05:26 -0600

    Getting this env variable setup early so I don't forget it later
    Signed-off-by: Elliot J. Voris <elliot@voris.me>

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,7 +1,7 @@
 const chai = require("chai");
 const chaiHttp = require('chai-http');
 const assert = chai.assert;
-const server = require('../app');
+const server = process.env.LIVE_URL || require('../app');
 
 chai.use(chaiHttp);
 
