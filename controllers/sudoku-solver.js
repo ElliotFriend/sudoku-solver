614a2d96e0419eba4926054dc79c2e34eb7b2a91
Elliot J. Voris <elliot@voris.me>
2021-11-28 22:14:04 -0600

    Pretty decent start on some of the business logic
    Signed-off-by: Elliot J. Voris <elliot@voris.me>

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,6 +1,10 @@
 class SudokuSolver {
 
   validate(puzzleString) {
+    const re = /^[1-9\.]{81}$/
+    if (puzzleString.length !== 81) return { error: 'Expected puzzle to be 81 characters long' }
+    if (!puzzleString.match(re)) return { error: 'Invalid characters in puzzle'}
+    return true
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
@@ -16,9 +20,9 @@ class SudokuSolver {
   }
 
   solve(puzzleString) {
-    
+    if (error) return { error: 'Puzzle cannot be solved' }
+    return { solution: 'somesolutionishere12834693....'}
   }
 }
 
 module.exports = SudokuSolver;
-
--- routes/api.js
+++ routes/api.js
@@ -3,16 +3,30 @@
 const SudokuSolver = require('../controllers/sudoku-solver.js');
 
 module.exports = function (app) {
-  
+
   let solver = new SudokuSolver();
 
   app.route('/api/check')
     .post((req, res) => {
+      if (!req.body.puzzle || !req.body.coordinate || !req.body.value) return res.json({ error: 'Required field(s) missing' })
+      let puzzle = req.body.puzzle
+      let coordinate = req.body.coordinate
+      let value = req.body.value
+      let isValid = solver.validate(puzzle)
+      if (isValid.error) return res.json({ error: isValid.error })
+      if (!/^[A-I]{1}[1-9]{1}$/i.test(coordinate)) return res.json({ error: 'Invalid coordinate' })
+      if (!/^[1-9]{1}$/.test(value)) return res.json({ error: 'invalid value' })
 
+      // we've passed all the validations, let's get to sudoku-in' now!
     });
-    
+
   app.route('/api/solve')
     .post((req, res) => {
+      if (!req.body.puzzle) return res.json({ error: 'Required field missing'})
+      let puzzle = req.body.puzzle
+      let isValid = solver.validate(puzzle)
+      if (isValid.error) return res.json({ error: isValid.error })
 
+      // we've passed all the validations, let's get to sudoku-in' now!
     });
 };
