565b21821d77080f9fd7a4be319ebaee07de25a9
Elliot J. Voris <elliot@voris.me>
2021-12-01 00:16:33 -0600

    Cranking out the first batch of functional tests
    Signed-off-by: Elliot J. Voris <elliot@voris.me>

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,78 +1,119 @@
 const chai = require("chai");
 const chaiHttp = require('chai-http');
+const { puzzlesAndSolutions } = require('../controllers/puzzle-strings.js')
+const { invalidPuzzles } = require('../controllers/invalid-puzzles.js')
 const assert = chai.assert;
 const server = process.env.LIVE_URL || require('../app');
+
 
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-  // this.timeout(5000)
-  //
-  // // /api/solve endpoint
-  // test('Solve a puzzle with valid puzzle string: POST request to /api/solve', () => {
-  //   chai.request(server)
-  //     .post('/api/solve')
-  //     .type('json')
-  //     .send({ puzzle: 'something' })
-  //     .end( (err, res) => {
-  //       assert.equal(true, false)
-  //       done()
-  //     })
-  // })
-  //
-  // test('Solve a puzzle with missing puzzle string: POST request to /api/solve', () => {
-  //   chai.request(server)
-  //     .post('/api/solve')
-  //     .type('json')
-  //     .send({ puzzle: 'something' })
-  //     .end( (err, res) => {
-  //       assert.equal(true, false)
-  //       done()
-  //     })
-  // })
-  //
-  // test('Solve a puzzle with invalid characters: POST request to /api/solve', () => {
-  //   chai.request(server)
-  //     .post('/api/solve')
-  //     .type('json')
-  //     .send({ puzzle: 'something' })
-  //     .end( (err, res) => {
-  //       assert.equal(true, false)
-  //       done()
-  //     })
-  // })
-  //
-  // test('Solve a puzzle with incorrect length: POST request to /api/solve', () => {
-  //   chai.request(server)
-  //     .post('/api/solve')
-  //     .type('json')
-  //     .send({ puzzle: 'something' })
-  //     .end( (err, res) => {
-  //       assert.equal(true, false)
-  //       done()
-  //     })
-  // })
-  //
-  // test('Solve a puzzle that cannot be solved: POST request to /api/solve', () => {
-  //   chai.request(server)
-  //     .post('/api/solve')
-  //     .type('json')
-  //     .send({ puzzle: 'something' })
-  //     .end( (err, res) => {
-  //       assert.equal(true, false)
-  //       done()
-  //     })
-  // })
+  this.timeout(5000)
+
+  suite('Testing the /api/solve endpoint', () => {
+
+    test('Solve a puzzle with valid puzzle string: POST request to /api/solve', (done) => {
+      chai.request(server)
+        .post('/api/solve')
+        .type('json')
+        .send({ puzzle: puzzlesAndSolutions[0][0] })
+        .end( (err, res) => {
+          assert.equal(res.status, 200, 'return status should be 200')
+          assert.isObject(res.body, 'returned object should be of type object')
+          assert.property(res.body, 'solution', 'returned object should contain a solution property')
+          assert.isString(res.body.solution, 'returned solution property should be of type string')
+          assert.equal(res.body.solution, puzzlesAndSolutions[0][1], 'returned solution should be the same as the known, valid solution')
+          done()
+        })
+    })
+
+    test('Solve a puzzle with missing puzzle string: POST request to /api/solve', (done) => {
+      chai.request(server)
+        .post('/api/solve')
+        .type('json')
+        .send({ fakeproperty: 'something' })
+        .end( (err, res) => {
+          assert.equal(res.status, 200, 'return status should be 200')
+          assert.isObject(res.body, 'returned object should be of type object')
+          assert.property(res.body, 'error', 'returned object should contain an error property')
+          assert.isString(res.body.error, 'returned error property should be of type string')
+          assert.equal(res.body.error, 'Required field missing', 'returned error should contain a helpful message')
+          done()
+        })
+    })
+
+    test('Solve a puzzle with invalid characters: POST request to /api/solve', (done) => {
+      chai.request(server)
+        .post('/api/solve')
+        .type('json')
+        .send({ puzzle: invalidPuzzles[0] })
+        .end( (err, res) => {
+          assert.equal(res.status, 200, 'return status should be 200')
+          assert.isObject(res.body, 'returned object should be of type object')
+          assert.property(res.body, 'error', 'returned object should contain an error property')
+          assert.isString(res.body.error, 'returned error property should be of type string')
+          assert.equal(res.body.error, 'Invalid characters in puzzle', 'returned error should contain a helpful message')
+          done()
+        })
+    })
+
+    test('Solve a puzzle with incorrect length: POST request to /api/solve', (done) => {
+      chai.request(server)
+        .post('/api/solve')
+        .type('json')
+        .send({ puzzle: invalidPuzzles[1] }) // puzzle too long
+        .end( (err, res) => {
+          assert.equal(res.status, 200, 'return status should be 200')
+          assert.isObject(res.body, 'returned object should be of type object')
+          assert.property(res.body, 'error', 'returned object should contain an error property')
+          assert.isString(res.body.error, 'returned error property should be of type string')
+          assert.equal(res.body.error, 'Expected puzzle to be 81 characters long', 'returned error should contain a helpful message')
+          chai.request(server)
+            .post('/api/solve')
+            .type('json')
+            .send({ puzzle: invalidPuzzles[2] }) // puzzle too short
+            .end( (err, res) => {
+              assert.equal(res.status, 200, 'return status should be 200')
+              assert.isObject(res.body, 'returned object should be of type object')
+              assert.property(res.body, 'error', 'returned object should contain an error property')
+              assert.isString(res.body.error, 'returned error property should be of type string')
+              assert.equal(res.body.error, 'Expected puzzle to be 81 characters long', 'returned error should contain a helpful message')
+              done()
+            })
+        })
+    })
+
+    test('Solve a puzzle that cannot be solved: POST request to /api/solve', (done) => {
+      chai.request(server)
+        .post('/api/solve')
+        .type('json')
+        .send({ puzzle: invalidPuzzles[3] })
+        .end( (err, res) => {
+          assert.equal(res.status, 200, 'return status should be 200')
+          assert.isObject(res.body, 'returned object should be of type object')
+          assert.property(res.body, 'error', 'returned object should contain an error property')
+          assert.isString(res.body.error, 'returned error property should be of type string')
+          assert.equal(res.body.error, 'Puzzle cannot be solved', 'returned error should contain a helpful message')
+          done()
+        })
+    })
+
+  })
 
 
-  // /api/check endpoint
-  // Check a puzzle placement with all fields: POST request to /api/check
-  // Check a puzzle placement with single placement conflict: POST request to /api/check
-  // Check a puzzle placement with multiple placement conflicts: POST request to /api/check
-  // Check a puzzle placement with all placement conflicts: POST request to /api/check
-  // Check a puzzle placement with missing required fields: POST request to /api/check
-  // Check a puzzle placement with invalid characters: POST request to /api/check
-  // Check a puzzle placement with incorrect length: POST request to /api/check
-  // Check a puzzle placement with invalid placement coordinate: POST request to /api/check
-  // Check a puzzle placement with invalid placement value: POST request to /api/check
+  suite('Testing the /api/check endpoint', () => {
+
+    // Check a puzzle placement with all fields: POST request to /api/check
+    // Check a puzzle placement with single placement conflict: POST request to /api/check
+    // Check a puzzle placement with multiple placement conflicts: POST request to /api/check
+    // Check a puzzle placement with all placement conflicts: POST request to /api/check
+    // Check a puzzle placement with missing required fields: POST request to /api/check
+    // Check a puzzle placement with invalid characters: POST request to /api/check
+    // Check a puzzle placement with incorrect length: POST request to /api/check
+    // Check a puzzle placement with invalid placement coordinate: POST request to /api/check
+    // Check a puzzle placement with invalid placement value: POST request to /api/check
+
+  })
+
 });
